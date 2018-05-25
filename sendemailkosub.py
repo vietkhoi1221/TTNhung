643094c0fb32c54e6992a980ef6f007370116b63
Khoi Nguyen <vietkhoi1221@gmail.com>
2018-05-25 17:30:06 +0700

    Update them thu vien

---
+++ sendemailkosub.py
@@ -0,0 +1,19 @@
+import smtplib
+host = "smtp.gmail.com"
+port = 587
+username = "vietkhoi1221@gmail.com"
+password = "khoidk00"
+from_email = username
+to_list = ["vietkhoi1221@gmail.com"]
+
+
+#email_conn = smtplib.SMTP(host, port)
+#email_conn.ehlo()
+#email_conn.starttls()
+#try:
+ #   email_conn.login(username,password)
+  #  email_conn.sendmail(from_email,to_list,"Day la mot cai email")
+#except:
+ #   print("Co loi roi, vui long thu lai")
+#email_conn.quit()
+
